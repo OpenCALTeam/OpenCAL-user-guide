6488d57fcced52bf6895c0f206fcd7b40381e8cd
knotman90 <davide90.spataro@gmail.com>
2016-02-02 16:22:26 +0000

    script that generates html sanitized+css version of the opencal manual

---
+++ generateHTML.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+mkdir -p html 
+
+pandoc --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML  --filter pandoc-crossref -s --toc --chapters -c github-pandoc.css  --default-image-extension=png -f latex -t html opencal-userguide.tex -o opencal-userguide_FULL.html
+
+
+htmldoc -t htmlsep -v -d html opencal-userguide_FULL.html
+
+mkdir -p html_sanitized
+#fix utf8
+HEADER=`cat header.html`
+echo "HEADER $HEADER"
+export HEAD=$HEADER
+cd html/
+for f in ./*.html;
+do 
+	echo "Processing $f file..";
+	python ../fix-htmldoc-utf8.py $f > ../html_sanitized/$f
+	
+	#add custom header: style and math javascriptv
+	perl -0777 -i.original -pe 's/<STYLE.*<\/STYLE>/$ENV{HEAD}/migs' ../html_sanitized/$f
+
+done
+
+cd ../html_sanitized
+cp -r ../images/ .
+cp ../github-pandoc.css .
